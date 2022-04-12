41e83bc5abd4c727b76e5a6f32aee6bd4203d36b
msheverdin648 <msheverdin648@gmail.com>
2022-04-12 15:42:40 +0300

    included debug_toolbar

--- qr_tatu/urls.py
+++ qr_tatu/urls.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from main.urls import urlpatterns as main_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(main_urls)),
+    path('', include('main.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
 
 
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG == True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
