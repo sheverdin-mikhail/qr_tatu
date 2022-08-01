c8107f1153f71ca4423511db2105394e72a9475b
msheverdin648 <msheverdin648@gmail.com>
2022-08-01 18:09:01 +0300

    verify e-mail

--- personal/utils.py
+++ personal/utils.py
@@ -3,7 +3,7 @@ import re
 from django.contrib.auth.tokens import default_token_generator as token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
-from django.template.loader import render_to_string
+from django.template.loader import render_to_string, get_template
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 
@@ -17,16 +17,17 @@ def send_email_for_verify(request, user, use_https=False):
         'token': token_generator.make_token(user),
         'protocol': 'https' if use_https else 'http',
     }
-
-    message = render_to_string(
-        'personal/verify_email.html',
-        context=context
-    )
+    message = get_template('personal/verify_email.html').render(context)
+    # message = render_to_string(
+    #     'personal/verify_email.html',
+    #     context=context
+    # )
     email = EmailMessage(
         'Verify email',
         message,
         to=[user.email],
     )
+    email.content_subtype = 'html'
     email.send()
 
 
