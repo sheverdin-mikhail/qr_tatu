928910cb8b415a53d1f7f49a6b42e16057e285ee
msheverdin648 <msheverdin648@gmail.com>
2022-09-02 17:54:33 +0300

    fix urls djnago admin panel

--- qr_tatu/urls.py
+++ qr_tatu/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', include('personal.urls')),
     path('', include('django.contrib.auth.urls')),
     path('qr_code/', include('qr_code.urls', namespace="qr_code")),
-    path('qr_tau_admin_panel/', admin.site.urls),
+    path('qr_tatu_admin_panel/', admin.site.urls),
     path('', include('main.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
